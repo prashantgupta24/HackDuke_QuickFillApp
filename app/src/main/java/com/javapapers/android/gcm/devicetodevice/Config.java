52f808c046a8edd2b28e5a38a68e9d8d1b6f3e92
prashantgupta24 <prashantgupta24@gmail.com>
2015-11-08 11:50:46 -0500

    initial commit

---
+++ app/src/main/java/com/javapapers/android/gcm/devicetodevice/Config.java
@@ -0,0 +1,19 @@
+package com.javapapers.android.gcm.devicetodevice;
+
+public interface Config {
+
+	// used to share GCM regId with application server - using php app server
+	//static final String APP_SERVER_URL = "http://192.168.1.17/gcm/gcm.php?shareRegId=1";
+
+	// GCM server using java
+	static final String APP_SERVER_URL = "http://172.30.5.82:8080/GCM-Server-Device-To-Device/GCMNotification?";
+
+	// Google Project Number
+	static final String GOOGLE_PROJECT_ID = "588800084950";
+	
+	static final String REGISTER_NAME = "name";
+	
+	static final String MESSAGE_KEY = "message";
+	static final String TO_NAME = "toName";	
+
+}
---
+++ app/src/main/java/com/javapapers/android/gcm/devicetodevice/MainActivity.java
@@ -0,0 +1,103 @@
+package com.javapapers.android.gcm.devicetodevice;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	ShareExternalServer appUtil;
+	String regId;
+	String userName;
+	AsyncTask<Void, Void, String> shareRegidTask;
+
+	EditText toUser;
+	EditText message;
+	Button btnSendMessage;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		appUtil = new ShareExternalServer();
+
+		regId = getIntent().getStringExtra("regId");
+		Log.d("MainActivity", "regId: " + regId);
+
+		userName = getIntent().getStringExtra(Config.REGISTER_NAME);
+		Log.d("MainActivity", "userName: " + userName);
+
+		shareRegidTask = new AsyncTask<Void, Void, String>() {
+			@Override
+			protected String doInBackground(Void... params) {
+				String result = appUtil
+						.shareRegIdWithAppServer(regId, userName);
+				return result;
+			}
+
+			@Override
+			protected void onPostExecute(String result) {
+				shareRegidTask = null;
+				Toast.makeText(getApplicationContext(), result,
+						Toast.LENGTH_LONG).show();
+			}
+
+		};
+
+		// to send message to another device via Google GCM
+		btnSendMessage = (Button) findViewById(R.id.sendMessage);
+		btnSendMessage.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+
+				toUser = (EditText) findViewById(R.id.toUser);
+				String toUserName = toUser.getText().toString();
+
+				message = (EditText) findViewById(R.id.message);
+				String messageToSend = message.getText().toString();
+
+				if (TextUtils.isEmpty(toUserName)) {
+					Toast.makeText(getApplicationContext(),
+							"To User is empty!", Toast.LENGTH_LONG).show();
+				} else if (TextUtils.isEmpty(messageToSend)) {
+					Toast.makeText(getApplicationContext(),
+							"Message is empty!", Toast.LENGTH_LONG).show();
+				} else {
+					Log.d("MainActivity", "Sending message to user: "
+							+ toUserName);
+					sendMessageToGCMAppServer(toUserName, messageToSend);
+
+				}
+			}
+		});
+
+		shareRegidTask.execute(null, null, null);
+	}
+
+	private void sendMessageToGCMAppServer(final String toUserName,
+			final String messageToSend) {
+		new AsyncTask<Void, Void, String>() {
+			@Override
+			protected String doInBackground(Void... params) {
+
+				String result = appUtil.sendMessage(userName, toUserName,
+						messageToSend);
+				Log.d("MainActivity", "Result: " + result);
+				return result;
+			}
+
+			@Override
+			protected void onPostExecute(String msg) {
+				Log.d("MainActivity", "Result: " + msg);
+				Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG)
+						.show();
+			}
+		}.execute(null, null, null);
+	}
+}
